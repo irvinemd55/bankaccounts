a83b53444f567ba3dae1edd889397ed924b27fc5
irvinemd55 <irvinedowning@gmail.com>
2017-11-01 16:40:03 -0700

    initial commit

---
+++ Models/Withdrawal.cs
@@ -0,0 +1,13 @@
+using System;
+namespace BankAccounts.Models
+{
+    public class Withdrawal
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public float Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
